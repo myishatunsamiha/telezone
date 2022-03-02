ed3646d843e4cddf694e9eee0242d7ffb3bcbb0f
myishatunsamiha <myisha.samiha@gmail.com>
2022-03-02 21:25:43 +0600

    added necessary comments to the js, css and html files.

--- js/script.js
+++ js/script.js
@@ -1,38 +1,15 @@
-const showAllResults = (phones) => {
-    const searchResulContainer = document.getElementById("search-result-container");
-
-    let row = document.querySelector(".row");
-
-    console.log("show phones after 19");
-    console.log(phones.slice(20, phones.length));
-    phones.slice(20, phones.length).forEach(phone => {
-        const col = document.createElement("col");
-        col.innerHTML = `<div class="card h-100 rounded-3 p-3">
-                                <img src="${phone.image}" class="card-img-top rounded" alt="image of the phone">
-                                    <div class="card-body">
-                                        <p class="card-title">Brand: <strong>${phone.brand}</strong></p>
-                                        <p class="card-text">Phone Name: <strong>${phone.phone_name}</strong></p>
-                                    </div>
-                                    <div class="d-flex justify-content-center">
-                                        <button type="button" class="btn btn-outline-success btn-lg" onclick=phoneDetails('${phone.slug}')>Explore</button>
-                                    </div>
-                            </div>`;
-        row.appendChild(col);
-    });
-    searchResulContainer.appendChild(row);
-}
-
-
+// function to toggle the display property of the loading spinner
 const toggleSpinner = displayStyle => {
     document.getElementById("spinner").style.display = displayStyle;
 }
 
+// function to toggle the display property of the search results container and phone details container
 const toggleSearchResult = displayStyle => {
     document.getElementById("search-result-container").style.display = displayStyle;
     document.getElementById("phone-detail-container").style.display = displayStyle;
 }
 
-
+// function to search phone by fetching the url, converting it to json object and display the results
 const searchPhone = () => {
     const searchField = document.getElementById("search-field");
     const searchFieldValue = searchField.value.toLowerCase();
@@ -53,25 +30,27 @@ const searchPhone = () => {
     toggleSearchResult("none");
 
     fetch(`https://openapi.programming-hero.com/api/phones?search=${searchFieldValue}`)     // fetch the url
-        .then(res => res.json())    // convert the response into json object
-        .then(phones => displayResults(phones.data))
+        .then(res => res.json())                         // convert the response into json object
+        .then(phones => displayResults(phones.data))    // display results by calling displayResults function
 
 }
+// handle the click event on the search-btn
 document.getElementById("search-btn").addEventListener("click", searchPhone);
 
 
+// display search results 
 const displayResults = (phones) => {
     const searchResultContainer = document.getElementById("search-result-container");
 
-    if (phones.length == 0) {
+    if (phones.length == 0) {   // if no phone is found
         searchResultContainer.innerHTML = `<h4 class="text-muted text-center">No results found!</h4>`;
 
-    } else {
+    } else {    // if phone is found
 
         let div = document.createElement("div");
         div.classList.add("row", "row-cols-1", "row-cols-md-3", "g-4");
 
-        phones.slice(0, 20).forEach(phone => {
+        phones.slice(0, 20).forEach(phone => {      // display first 20 or less phones
             const col = document.createElement("col");
             col.innerHTML = `<div class="card h-100 rounded-3 p-3">
                                 <img src="${phone.image}" class="card-img-top rounded" alt="image of the phone">
@@ -87,7 +66,7 @@ const displayResults = (phones) => {
         });
         searchResultContainer.appendChild(div);
 
-        if (phones.length > 20) {
+        if (phones.length > 20) {       // display the show button and the remaining phones when the show button will be clicked if the count of phones is more than 20 
             const showAllBtnDiv = document.createElement("div");
             showAllBtnDiv.classList.add("d-flex", "justify-content-center", "mt-3", "d-block");
             showAllBtnDiv.innerHTML = `<button type="button" class="btn btn-outline-success btn-lg" id="show-all-results-btn">Show All</button>`;
@@ -118,19 +97,22 @@ const displayResults = (phones) => {
 
     }
 
+    // toggle the spinner to display none when all the results have been displayed and make the search results visible
     toggleSpinner("none");
     toggleSearchResult("block");
 }
 
+// function to get the detail of the targeted phone by fetching the url, convert the response to json and display the phone detail
 const phoneDetails = (phoneId) => {
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
     console.log(url);
 
     fetch(url)
         .then(response => response.json())
-        .then(data => displayPhoneDetails(data.data));
+        .then(data => displayPhoneDetails(data.data));  // calling the displayPhoneDetail function to display detail of the phone
 }
 
+// function to display the detail of the phone
 const displayPhoneDetails = (phone) => {
 
     let releaseDateStatus;
@@ -159,7 +141,7 @@ const displayPhoneDetails = (phone) => {
                     </ul>
                     <br>
 
-                    ${(phone.others) ? `Others: 
+                    ${(phone.others) ? `Others: <br/>
                     <ul>
                         <li>WLAN: ${phone.others.WLAN ?? ""}</li>
                         <li>Bluetooth:${phone.others.Bluetooth ?? ""}</li>
@@ -170,7 +152,7 @@ const displayPhoneDetails = (phone) => {
                     </ul>` : ""}
                     
 
-    <a href="#" class="btn btn-outline-light">Purchase</a>
+                    <a href="#" class="btn btn-outline-light">Purchase</a>
                 </div >
                 </div > `
 };
